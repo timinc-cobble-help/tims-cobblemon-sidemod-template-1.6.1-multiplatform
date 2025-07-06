b45136d70e9109f1a5a252d4977b34ca532c8d67
Tim M <82600217+tmetcalfe89@users.noreply.github.com>
2025-07-06 02:13:07 +0000

    abstract MOD_ID

--- common/src/main/kotlin/{{package.original}}/{{sideMod.lowerCase}}/{{sideMod.upperCase}}.kt
+++ common/src/main/kotlin/{{package.original}}/{{sideMod.lowerCase}}/{{sideMod.upperCase}}.kt
@@ -3,7 +3,7 @@ package {{package.original}}.{{sideMod.lowerCase}}
 import us.timinc.mc.cobblemon.timcore.AbstractConfig
 import us.timinc.mc.cobblemon.timcore.AbstractMod
 
-const val MOD_ID: String = "capture_xp"
+const val MOD_ID: String = "{{sideMod.snakeCase}}"
 
 object {{sideMod.upperCase}} : AbstractMod<{{sideMod.upperCase}}.{{sideMod.upperCase}}Config>(MOD_ID, {{sideMod.upperCase}}Config::class.java) {
 
