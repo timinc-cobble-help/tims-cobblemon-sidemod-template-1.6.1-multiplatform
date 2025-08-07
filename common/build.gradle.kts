4d936186995b344a3e0e08072e8a8c6fcfbe9420
Tim M <82600217+tmetcalfe89@users.noreply.github.com>
2025-08-07 19:51:01 +0000

    bump to latest tim core on modrinth

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -16,8 +16,7 @@ dependencies {
     mappings(loom.officialMojangMappings())
     modImplementation("com.cobblemon:mod:${property("cobblemon_version")}") { isTransitive = false }
 
-    // TODO: Change to Modrinth maven.
-    modImplementation(files("libs/common-1.6.1-1.0.0.jar"))
+    modImplementation("maven.modrinth:cobblemon-tim-core:${property("tim_core_fabric_version")}")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit_version")}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${property("junit_version")}")
